aed2aecce4fdc8670d20df0eb26c91fe47cdb007
akskas <ankulkumar001@gmail.com>
2016-02-15 12:20:49 +0530

    added log

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ describe("Index", function () {
   it("renders HTML", function (done) {
     superagent.get("http://localhost:3000/")
       .end(function (e, res) {
+          console.log('e: ', e);
         (e === null).should.equal(true);
         res.text.should.equal("Hey buddy!");
         done();
